23512f0975b3b309f5900a91e35b3981799f37dc
stefanclarke66 <48825545+stefanclarke66@users.noreply.github.com>
2019-03-26 11:05:11 +0000

    Add files via upload

--- website_main.py
+++ website_main.py
@@ -4,6 +4,35 @@ import datetime
 install(SQLitePlugin(dbfile='library.db'))
 now = datetime.datetime.now()
 
+def check_book_exists(db, book_id):
+    book = db.execute('SElECT * FROM book WHERE id = ?', (book_id,)).fetchall()
+    if book == []:
+        return False
+    else:
+        return True
+
+def check_user_exists(db, user_id):
+    book = db.execute('SElECT * FROM user WHERE id = ?', (user_id,)).fetchall()
+    if book == []:
+        return False
+    else:
+        return True
+
+def check_loan_exists(db, user_id, book_id):
+    loan = db.execute('SELECT * FROM loan WHERE( user_id = ? AND book_id = ? AND out = 1)', (user_id, book_id)).fetchall()
+    if loan == []:
+        return False
+    else:
+        return True
+
+def check_book_available(db, book_id):
+    book = db.execute('SElECT * FROM book WHERE id = ?', (book_id,)).fetchone()
+    in_lib = int(book['in_library'])
+    if in_lib == 0:
+        return False
+    else:
+        return True
+
 @get('/views/<file:path>')
 def serve_static(file):
     return static_file(file, root='./views')
@@ -128,24 +157,70 @@ def finduser(db):
 def check_in(db):
     user_id = request.forms.get('user-id-in')
     book_id = request.forms.get('book-id-in')
-    db.execute('UPDATE book SET in_library = 0 WHERE id = ?', (book_id,))
+
+    user_existence = check_user_exists(db, user_id)
+    book_existence = check_book_exists(db, book_id)
+    loan_existence = check_loan_exists(db, user_id, book_id)
+
+    if user_existence == False:
+        message = 'This user does not exist!'
+        return template('in-out', message = message)
+
+    if book_existence == False:
+        message = 'This book does not exist!'
+        return template('in-out', message = message)
+
+    if loan_existence == False:
+        message = 'This user has not rented out this book!'
+        return template('in-out', message = message)
+
+    db.execute('UPDATE book SET in_library = 1 WHERE id = ?', (book_id,))
     book = db.execute('SELECT title FROM book WHERE id = ?', (book_id,)).fetchone()
     loan = db.execute('SELECT MAX(id) FROM (SELECT * FROM loan WHERE( book_id = ? AND user_id = ?))', (user_id, book_id)).fetchone()
     id = loan[0]
     db.execute('UPDATE loan SET out = 0 WHERE id = ?', (id,))
-
+    due_date = db.execute('SELECT due_date FROM loan WHERE id = ?', (id,)).fetchone()[0]
+    due_date1 = due_date[0:10]
+    due_date2 = datetime.datetime.strptime(due_date1, "%Y-%m-%d")
+    if due_date2 < datetime.datetime.now():
+        delta = datetime.datetime.now() - due_date2
+        days_late = delta.days
+        current_fine = db.execute('SELECT fines FROM user WHERE id = ?', (user_id,)).fetchone()[0]
+        current_fine1 = int(current_fine)
+        new_fine = current_fine1 + days_late
+        db.execute('UPDATE user SET fines = ? WHERE id = ?', (new_fine, user_id))
+        finestring = f'{days_late} DAYS LATE.'
+    else:
+        finestring = ''
     bookname = book['title']
-    message = f'Returning {bookname}'
+    message = f'Returning {bookname} ' + finestring
     return template('in-out', message = message)
 
 @post('/in-out/out')
 def check_out(db):
     user_id = request.forms.get('user-id-out')
     book_id = request.forms.get('book-id-out')
+
+    user_existence = check_user_exists(db, user_id)
+    book_existence = check_book_exists(db, book_id)
+    book_available = check_book_available(db, book_id)
+
+    if user_existence == False:
+        message = 'This user does not exist!'
+        return template('in-out', message = message)
+
+    if book_existence == False:
+        message = 'This book does not exist!'
+        return template('in-out', message = message)
+
+    if book_available == False:
+        message = 'This book is already rented out!'
+        return template('in-out', message = message)
+    
     time_out = request.forms.get('time-out')
     time_out = int(time_out)
     day_due = datetime.datetime.now() + datetime.timedelta(days = time_out)
-    db.execute('UPDATE book SET in_library = 1 WHERE id = ?', (book_id,))
+    db.execute('UPDATE book SET in_library = 0 WHERE id = ?', (book_id,))
     db.execute('INSERT INTO loan (user_id, book_id, out_date, due_date, out, reserved) VALUES (?, ?, ?, ?, ?, ?) ', (user_id, book_id, datetime.datetime.now(), day_due, 1, 0))
     book_name = db.execute('SELECT * FROM book WHERE id = ?', (book_id)).fetchone()
     book_name = book_name['title']
@@ -167,7 +242,7 @@ def findbook(db):
         author_second_name = db.execute('SELECT last_name FROM author WHERE id = ?', (author_id,)).fetchone()[0]
         author_name = author_first_name + " " + author_second_name
         book_id = book['id']
-        if book['in_library'] == 1:
+        if book['in_library'] == 0:
             out = 'Yes'
             loan_id = db.execute('SELECT MAX(id) FROM (SELECT * FROM loan WHERE( book_id = ?))', (book_id,)).fetchone()[0]
             due_date = db.execute('SElECT due_date FROM loan WHERE id = ?', (loan_id,)).fetchone()[0]
@@ -184,20 +259,20 @@ def findbook(db):
 @post('/fines/fine')
 def fineuser(db):
     user_id = request.forms.get('user-id-in')
+    user = db.execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
+    return template('fine-user', user_ID = user['id'], user_first_name = user['first_name'], user_second_name = user['second_name'], user_fine = user['fines'], message = '')
 
-
-
-
-
-
-    
-
-
-
-    
-
-
-
-
+@post('/fines/fine/submit')
+def submitfine(db):
+    user_id = request.forms.get('user_id')
+    fine_paid = request.forms.get('fine_value')
+    user = db.execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
+    current_fine = user['fines']
+    paid = int(fine_paid)
+    new_fine = current_fine - paid
+    db.execute('UPDATE user SET fines = ? WHERE id = ?', (new_fine, user_id))
+    db.commit()
+    message = f"User {user['first_name']} {user['second_name']} paid £{fine_paid} in fines."
+    return template('fine-user', user_ID = user_id, user_first_name = user['first_name'], user_second_name = user['second_name'], user_fine = new_fine, message = message)
 
 run(host='localhost', port=8080, debug = True)
